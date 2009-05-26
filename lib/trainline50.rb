0ee722af216da88412ee08eb88dcae8925dd1ce8
jebw <jeb@jdwilkins.co.uk>
2009-05-27 00:46:52 +0100

    tests to check methods are being added

--- lib/trainline50.rb
+++ lib/trainline50.rb
@@ -1,1 +1,19 @@
-# Trainline50
+module Trainline50
+	
+	def sage_object(object_type, fields, map, options)
+	
+		define_method "to_sage_xml" do
+		end
+		
+		define_method "from_sage_xml" do
+		end
+		
+		class << self
+			define_method "sync_with_sage" do
+			end
+		end
+		
+	end
+	
+end
+
--- test/trainline50_test.rb
+++ test/trainline50_test.rb
@@ -1,8 +1,29 @@
 require 'test_helper'
 
+class MockModel
+	attr_accessor :firstname, :lastname, :age, :dob
+	extend Trainline50
+	sage_object :sage_test, { :firstname => [], :surname => [ :lastname ] }, {}, {}
+	
+	def responds_to?(method)
+		public_methods.include?(method.to_s)
+	end
+end
+
 class Trainline50Test < ActiveSupport::TestCase
-  # Replace this with your real tests.
-  test "the truth" do
-    assert true
+  test "sage_object method exists" do
+    assert MockModel.public_methods.include?('sage_object')
+  end
+  
+  test "MockModel instance has to_sage_xml method" do
+  	assert MockModel.new.responds_to?(:to_sage_xml)
+  end
+  
+  test "MockModel instance has from_sage_xml method" do
+  	assert MockModel.new.responds_to?(:from_sage_xml)
+  end
+  
+  test "MockModel class has a sync_with_sage method" do
+  	assert MockModel.public_methods.include?('sync_with_sage')
   end
 end
