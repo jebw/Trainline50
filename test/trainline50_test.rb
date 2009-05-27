84af575e02115c39dc4eefaf1e5d27e0968d8186
jebw <jeb@jdwilkins.co.uk>
2009-05-27 15:39:33 +0100

    moved mocks into their own file

--- test/trainline50_test.rb
+++ test/trainline50_test.rb
@@ -1,58 +1,5 @@
 require 'test_helper'
-
-class MockModel
-	attr_accessor :firstname, :lastname, :age, :dob
-	extend Trainline50
-	
-	def initialize(values = {})
-		values.each do |key, value|
-			__send__("#{key.to_s}=", value)
-		end
-	end
-	
-	def responds_to?(method)
-		public_methods.include?(method.to_s)
-	end
-end
-
-class SimpleMock < MockModel
-	sage_object :sage_test, { :firstname => [], :surname => [] }, {}, { :primary => :firstname }
-end
-
-class AlternativesMock < MockModel
-	sage_object :sage_test, { :firstname => [], :surname => [ :secondname, :lastname ] }, {}, { :primary => :firstname }
-end
-
-class UserMapMock < MockModel
-	sage_object :sage_test, { :firstname => [], :surname => [ :lastname ] }, 
-								{ :firstname => :lastname, :surname => :firstname }, { :primary => :firstname }
-end
-
-class UserMapMockWithStatics < MockModel
-	sage_object :sage_test, { :firstname => [], :code => [], :type => [] }, 
-								{ :code => 17, :type => 'Foo' }, { :primary => :firstname }
-end
-
-class UserMapMockWithProc < MockModel
-	sage_object :sage_test, { :firstname => [], :proctest => []}, 
-								{ :proctest => Proc.new { |o| "#{o.firstname} #{o.lastname}" } }, { :primary => :firstname}
-end
-
-class ParentMock < MockModel
-	attr_accessor :name, :child
-	sage_object :sage_test, { :parent => [], :child => [], :grandchild => []},
-									{ :parent => :name, :child => :child__name, :grandchild => :child__child__name }, { :primary => :firstname}
-	
-	def initialize(*args)
-		super(*args)
-		self.name = 'parent'
-		self.child = ChildMock.new(:name => 'child', :child => ChildMock.new(:name => 'grandchild'))
-	end
-end
-
-class ChildMock < MockModel
-	attr_accessor :name, :child
-end
+require File.join(File.dirname(__FILE__), 'mocks.rb')
 
 class Trainline50Test < ActiveSupport::TestCase
   test "sage_object method exists" do
